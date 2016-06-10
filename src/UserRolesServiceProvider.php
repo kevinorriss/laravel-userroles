58a0af0bf7eed92e2a55500d4aaa53c85c66124b
Kevin Orriss <kevinorriss@gmail.com>
2016-06-10 13:04:07 +0100

    Updated service provider namespaces
    For controllers

--- src/UserRolesServiceProvider.php
+++ src/UserRolesServiceProvider.php
@@ -30,7 +30,7 @@ class UserRolesServiceProvider extends ServiceProvider
     public function register()
     {
         include __DIR__ . '/routes.php';
-        $this->app->make('KevinOrriss\UserRoles\RoleController');
-        $this->app->make('KevinOrriss\UserRoles\RoleGroupController');
+        $this->app->make('KevinOrriss\UserRoles\App\Http\Controllers\RoleController');
+        $this->app->make('KevinOrriss\UserRoles\App\Http\Controllers\RoleGroupController');
     }
 }
