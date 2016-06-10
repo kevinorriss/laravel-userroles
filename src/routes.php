231a07abf2f29cb54812c61bf193853b06e44b5f
Kevin Orriss <kevinorriss@gmail.com>
2016-06-10 13:36:15 +0100

    route middleware
    added web and auth middleware to the resource routes

--- src/routes.php
+++ src/routes.php
@@ -1,4 +1,4 @@
 <?php
 
-Route::resource('role', 'KevinOrriss\UserRoles\Controllers\RoleController');
-Route::resource('role_group', 'KevinOrriss\UserRoles\Controllers\RoleGroupController');
+Route::resource('role', 'KevinOrriss\UserRoles\Controllers\RoleController')->middleware(['web', 'auth']);
+Route::resource('role_group', 'KevinOrriss\UserRoles\Controllers\RoleGroupController')->middleware(['web', 'auth']);
