d39e34c0ad3bbaf24e80014bffb777bb95de98dc
Kevin Orriss <kevinorriss@gmail.com>
2016-06-16 21:43:12 +0100

    Changed role name max length

--- database/migrations/2016_06_08_184527_create_roles_table.php
+++ database/migrations/2016_06_08_184527_create_roles_table.php
@@ -23,7 +23,7 @@ class CreateRolesTable extends Migration
 
         if (DB::connection()->getDriverName() == 'pgsql')
         {
-            DB::statement('ALTER TABLE roles ADD CONSTRAINT name_length CHECk(length(name) BETWEEN 3 AND 20)');
+            DB::statement('ALTER TABLE roles ADD CONSTRAINT name_length CHECk(length(name) BETWEEN 3 AND 50)');
             DB::statement('ALTER TABLE roles ADD CONSTRAINT name_format CHECK(name ~ \'^[a-z]+(_[a-z]+)*$\')');
             DB::statement('ALTER TABLE roles ADD CONSTRAINT description_length CHECK (length(description) > 10)');
         }
