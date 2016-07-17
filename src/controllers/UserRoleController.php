71361b44feb640bb22bcb2d489fa356f85207853
Kevin Orriss <kevinorriss@gmail.com>
2016-07-17 18:56:32 +0100

    added Controller use to UserRoleController

--- src/controllers/UserRoleController.php
+++ src/controllers/UserRoleController.php
@@ -5,6 +5,7 @@ namespace KevinOrriss\UserRoles\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Http\Controllers\Controller;
 
 class UserRoleController extends Controller
 {
